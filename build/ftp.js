5b3eba364f2490ab37c5f8b01a1fd736aac51290
Josef Biehler <gabbersepp@googlemail.com>
2020-01-11 10:50:16 +0100

    add ftp upload

---
+++ build/ftp.js
@@ -0,0 +1,23 @@
+// take all from ./page and upload it
+const ftp = require("basic-ftp")
+ 
+upload()
+ 
+async function upload() {
+    const client = new ftp.Client();
+    client.ftp.verbose = true;
+
+    try {
+        await client.access({
+            host: process.env.FTP_SERVER,
+            user: process.env.FTP_USER,
+            password: process.env.FTP_PWD,
+            secure: true
+        });
+        await client.uploadFromDir("./page/dist", "./");
+    } catch(err) {
+        console.log(err);
+    }
+
+    client.close();
+}
