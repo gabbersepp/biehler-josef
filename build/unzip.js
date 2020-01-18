0c96d992a8e2e6a1a28fde37accf9c1f30eb1568
Josef Biehler <gabbersepp@googlemail.com>
2020-01-18 11:47:59 +0100

    replace ftp with http upload

--- build/unzip.js
+++
@@ -1,9 +0,0 @@
-var axios = require("axios");
-
-axios.get('https://biehler-josef.de/zip.php')
-  .then(response => {
-    console.log(response.data);
-  })
-  .catch(error => {
-    console.log(error);
-  });
