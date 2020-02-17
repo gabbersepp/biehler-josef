9ff88a31f1d6e37b14998edc859d37c741310646
Josef Biehler <gabbersepp@googlemail.com>
2020-02-17 22:29:30 +0100

    use new lib to download twitter stuff

--- build/tweets.js
+++ build/tweets.js
@@ -1,84 +1,24 @@
-const Twitter = require("twitter-lite");
-const fs = require("fs");
-const download = require("./downloader.js")
+const tweetDownloader = require("tweet-downloader");
 
 async function getLatestEarningsPost() {
-    const existingTweets  = readTweets();
-    const ids = existingTweets.map(x => x.id).sort().reverse();
-    const maxId = ids.length > 0 ? ids[0] : 1;
-
-    const client = new Twitter({
-        subdomain: "api",
-        consumer_key: process.env.TWTR_CKEY,
-        consumer_secret: process.env.TWTR_CSECRET,
-        access_token_key: process.env.TWTR_ATOKEN,
-        access_token_secret: process.env.TWTR_ASECRET
-    });
-
-    let timeline;
+    let tweets = tweetDownloader.readTweets("./data/tweets.json");
     
-    timeline = await client.get("statuses/user_timeline", {
-        screen_name: "JosefBiehler",
-        exclude_replies: true,
-        include_rts: false,
-        tweet_mode: "extended",
-        count: 200,
-        since_id: maxId.toString()
-    });
-
-    for (var i = 0; i < timeline.length; i++) {
-        let tweet = timeline[i];
-        timeline[i] = await client.get(`statuses/show/${tweet.id_str}`, {
-            tweet_mode: "extended"
-        });
+    let maxId = BigInt(1);
+    if (tweets.length > 0) {
+        maxId = tweets.map(x => x.id).reduce((x, y) => x > y ? x : y);
     }
+    
+    const jbTweets = await tweetDownloader.getLatestEarningsPost(maxId.toString(), "JosefBiehler", "./data/images",
+        process.env.TWTR_CKEY, process.env.TWTR_CSECRET, process.env.TWTR_ATOKEN, process.env.TWTR_ASECRET);
 
-    const getHashtags = x => {
-        if (x.entities && x.entities.hashtags) {
-            return x.entities.hashtags.map(x => x.text);
-        }
-        return [];
-    }
+    const kackDevTweets = await tweetDownloader.getLatestEarningsPost(maxId.toString(), "KackDev", "./data/images",
+        process.env.TWTR_CKEY, process.env.TWTR_CSECRET, process.env.TWTR_ATOKEN, process.env.TWTR_ASECRET);
 
-    const results = timeline.map(x => ({
-        fullText: x.full_text,
-        createdAt: x.created_at,
-        id: x.id_str,
-        mediaUrl: x.extended_entities ? x.extended_entities.media[0].media_url : null,
-        hashtags: getHashtags(x)
-    }));
+    tweets = [...jbTweets, ...kackDevTweets];
 
-    for (var i = 0; i < results.length; i++) {
-        const x = results[i];
-        if (!x.mediaUrl) {
-            continue;
-        }
-        const path = await download(x.mediaUrl, x.id);
-        x.twitterMediaUrl = x.mediaUrl;
-        x.mediaUrl = path;
-    }
+    tweetDownloader.mergeAndWriteWithExisting("./data/tweets.json", tweets);
 
-    const merged = [...existingTweets, ...results]
-        .map(x => {
-            x.id = BigInt(x.id)
-            return x;
-        })
-        .sort((a, b) => a.id < b.id ? 1 : -1)
-    	.map(x => {
-            x.id = x.id.toString();
-            return x;
-        });
-
-    fs.writeFileSync("./data/tweets.json", JSON.stringify(merged));
-
-    return merged;
-}
-
-function readTweets() {
-    return JSON.parse(fs.readFileSync("./data/tweets.json").toString()).map(x => {
-        x.id = BigInt(x.id);
-        return x;        
-    });
+    return tweets;
 }
 
 getLatestEarningsPost().catch(e => {
