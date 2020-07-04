c453407cb3be813222fd8e6aacc2407288593098
gabbersepp <gabbersepp@googlemail.com>
2020-07-04 10:32:05 +0200

    add maxretry option. it seems that it takes very long on travis

--- build/githubcloud.js
+++ build/githubcloud.js
@@ -5,7 +5,7 @@ const path = require("path");
 var a = path.resolve(path.resolve("./temp/cloud"));
 
 async function run() {
-    await gcc.generateCloud("gabbersepp", process.env.GITHUB_TOKEN, ["js", "asm", "cs", "ts", "java", "cpp"], path.resolve("./temp/cloud"), 0.8, { backgroundColor: "white", height: 800 });
+    await gcc.generateCloud("gabbersepp", process.env.GITHUB_TOKEN, ["js", "asm", "cs", "ts", "java", "cpp"], path.resolve("./temp/cloud"), 0.8, { maxRetry: 10000, backgroundColor: "white", height: 800 });
     fs.copyFileSync("./temp/cloud/export/img.png", "./assets/codecloud.png");
 }
 
