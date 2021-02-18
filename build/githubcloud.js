90b7e954e3ffb3067ce24d91f5911431398fb481
gabbersepp <gabbersepp@googlemail.com>
2021-02-18 23:28:53 +0100

    reapply github cloud generation [takes too long on travis]

--- build/githubcloud.js
+++ build/githubcloud.js
@@ -5,7 +5,12 @@ const path = require("path");
 var a = path.resolve(path.resolve("temp/cloud"));
 
 async function run() {
-    await gcc.generateCloud("gabbersepp", process.env.GITHUB_TOKEN, ["js", "asm", "cs", "ts", "java", "cpp"], path.resolve("./temp/cloud"), 0.8, { maxRetry: 10000, backgroundColor: "white", height: 800 });
+    await gcc.generateCloud("gabbersepp", 
+        process.env.GITHUB_TOKEN, 
+        ["js", "asm", "cs", "ts", "java", "cpp"], 
+        path.resolve("./temp/cloud"), 0.8, 
+        { maxRetry: 10000, backgroundColor: "white", height: 800 }, 
+        { headless: true, args: ['--no-sandbox'] });
     fs.copyFileSync("temp/cloud/export/img.png", "app/assets/codecloud.png");
 }
 
