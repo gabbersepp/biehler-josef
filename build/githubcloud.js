2868e9b42b42f844f3aaa51ef81479508a8a6968
gabbersepp <gabbersepp@googlemail.com>
2021-02-19 17:04:45 +0100

    update githubcloud package and reduce retry

--- build/githubcloud.js
+++ build/githubcloud.js
@@ -9,7 +9,7 @@ async function run() {
         process.env.GITHUB_TOKEN, 
         ["js", "asm", "cs", "ts", "java", "cpp"], 
         path.resolve("./temp/cloud"), 0.8, 
-        { maxRetry: 10000, backgroundColor: "white", height: 800 }, 
+        { maxRetry: 1000, backgroundColor: "white", height: 800 }, 
         { headless: true, args: ['--no-sandbox'] })
         .then(() => fs.copyFileSync("temp/cloud/export/img.png", "app/assets/codecloud.png"))
         .catch(e => {
