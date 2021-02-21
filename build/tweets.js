769b61b3904d67f2d7a55073549ab7ea2eec04f2
gabbersepp <gabbersepp@googlemail.com>
2021-02-21 23:11:12 +0100

    Revert "try to fix pathes"
    This reverts commit 22f51333aeb546840090b5467d915138ccfba7f9.

--- build/tweets.js
+++ build/tweets.js
@@ -1,38 +1,32 @@
 const tweetDownloader = require("tweet-downloader");
 const fs = require("fs");
-const path = require("path");
-
-const basePath = path.resolve(".");
-const imagesBasePath = `${basePath}/app/data/images`;
-const tweetsBasePath = `${basePath}/app/data/tweets.json`;
 
 async function getLatestTweets() { 
-    let tweets = tweetDownloader.readTweets(tweetsBasePath);
+    let tweets = tweetDownloader.readTweets("app/data/tweets.json");
     
     let maxId = BigInt(1);
     if (tweets.length > 0) {
         maxId = tweets.map(x => x.id).reduce((x, y) => x > y ? x : y);
     }
     
- 
-    const jbTweets = await tweetDownloader.getLatestTweets(maxId.toString(), "JosefBiehler", imagesBasePath,
+    const jbTweets = await tweetDownloader.getLatestTweets(maxId.toString(), "JosefBiehler", "app/data/images",
         process.env.TWTR_CKEY, process.env.TWTR_CSECRET, process.env.TWTR_ATOKEN, process.env.TWTR_ASECRET, 3200);
 
-    const kackDevTweets = await tweetDownloader.getLatestTweets(maxId.toString(), "KackDev", imagesBasePath,
+    const kackDevTweets = await tweetDownloader.getLatestTweets(maxId.toString(), "KackDev", "app/data/images",
         process.env.TWTR_CKEY, process.env.TWTR_CSECRET, process.env.TWTR_ATOKEN, process.env.TWTR_ASECRET, 3200);
 
     tweets = [...jbTweets, ...kackDevTweets];
 
-    tweetDownloader.mergeAndWriteWithExisting(tweetsBasePath, tweets);
+    tweetDownloader.mergeAndWriteWithExisting("app/data/tweets.json", tweets);
 
     return tweets;
 }
 
 async function getSpecificTweets() {
-    const tweetIds = JSON.parse(fs.readFileSync(`${basePath}/app/data/devto-to-tweet.json`).toString()).map(x => x.tweetId);
-    const tweets = await tweetDownloader.getSpecificTweets(tweetIds, imagesBasePath,
+    const tweetIds = JSON.parse(fs.readFileSync("app/data/devto-to-tweet.json").toString()).map(x => x.tweetId);
+    const tweets = await tweetDownloader.getSpecificTweets(tweetIds, "app/data/images",
     process.env.TWTR_CKEY, process.env.TWTR_CSECRET, process.env.TWTR_ATOKEN, process.env.TWTR_ASECRET);
-    tweetDownloader.mergeAndWriteWithExisting(tweetsBasePath, tweets);
+    tweetDownloader.mergeAndWriteWithExisting("app/data/tweets.json", tweets);
 }
 
 getLatestTweets()
